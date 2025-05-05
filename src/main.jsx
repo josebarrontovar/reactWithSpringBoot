61be7d708d4284676ea9eb63f00bc42fb3cd77d9
josebarrontovar <josebarrontovar@hotmail.com>
2025-05-05 09:40:08 -0600

    remove useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Contador } from './components/Contador';
-import { HiddeMessage } from './components/HiddeMessage';
-import { ListTask } from './components/listTask';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Contador/>
-        <HiddeMessage/>
-        <ListTask/>
+       
     </React.StrictMode>
 )
